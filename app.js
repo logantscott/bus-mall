5637a58e5879c79a85a51ba480ffbbb8d1c3e919
Logan Scott <logantscott@gmail.com>
2020-01-28 09:51:52 -0800

    planning

---
+++ app.js
@@ -0,0 +1,28 @@
+//sessionStorage.getItem('')
+
+//onclick sessionstorage counter++
+//if checkcounter25(), get session key, clear session storage, and load session results
+
+//so, on survey page load, get session key and counter to vars
+//on click, sessionstorage increase counter, checkcounter25() (if yes, load session key analytics)
+
+//session object:
+//{ 
+    // productIds: [1, 5, 15],
+    // selected: 15,
+    // sessionId: 234213534532
+    // clickId?
+//}
+// get unique sessions?
+// get id appearance totals from session productIds property
+// get selected total from session selected property
+
+
+//for comparing, store a previous session key separately
+//function will be new obj without previous session obj ids
+//stretch goal: prefer showing products with less representation?
+//selection random... how to iterate and remove random selection for showing 3 unique non-repetitive products?
+
+//difference between 'session of 25' and click session... make sure to differentiate
+//session of 25 can use a separate var for keeping track... maybe even in sessionStorage
+//click session just gets recorded to total sessions (.push(), .setItem())
