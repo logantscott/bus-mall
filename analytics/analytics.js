458ccf4bedffd9695eb67561b9ccb8bf9e064d99
Logan Scott <logantscott@gmail.com>
2020-01-29 03:44:10 -0800

    analytics table is sortable, and has 'totals'

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -19,45 +19,114 @@ allSessions.forEach(session => {
 
 // console.log(surveyResults);
 
-//RENDER__________________________________
-const container = document.getElementById('analyticsContainer');
+renderAnalyticsTable();
 
-const table = document.createElement('table');
-const tableHeader = document.createElement('thead')
-const tableHeaderRow = document.createElement('tr');
-const productTH = document.createElement('th');
-const impressionsTH = document.createElement('th');
-const clicksTH = document.createElement('th');
-const percentageTH = document.createElement('th');
+function renderAnalyticsTable(sortResults = Object.keys(surveyResults)) {
+    //RENDER__________________________________
+    const container = document.getElementById('analyticsContainer');
 
-productTH.textContent = 'Products';
-impressionsTH.textContent = 'Impressions';
-clicksTH.textContent = 'Clicks';
-percentageTH.textContent = 'Percentage';
+    const table = document.createElement('table');
+    table.id = 'analyticsTable';
 
-tableHeaderRow.append(productTH, impressionsTH, clicksTH, percentageTH);
-tableHeader.appendChild(tableHeaderRow);
-table.appendChild(tableHeader);
+    const tableHeader = document.createElement('thead');
+    const tableHeaderRow = document.createElement('tr');
+    const headers = ['Products', 'Impressions', 'Clicks', 'Percentage'];
 
-const tableBody = document.createElement('tbody');
+    headers.forEach(header => {
+        const th = document.createElement('th');
+        th.textContent = header;
+        th.addEventListener('click', () => {
+            sortAnalytics(header.toLowerCase());
+        });
+        tableHeaderRow.appendChild(th);
+    });
 
-Object.keys(surveyResults).forEach(key => {
-    const tr = document.createElement('tr');
-    const product = document.createElement('td');
-    const impressions = document.createElement('td');
-    const clicks = document.createElement('td');
-    const percentage = document.createElement('td');
+    tableHeader.appendChild(tableHeaderRow);
+    table.appendChild(tableHeader);
 
-    // console.log(key);
+    const tableBody = document.createElement('tbody');
 
-    product.textContent = key;
-    impressions.textContent = surveyResults[key].impressions ? surveyResults[key].impressions : 0;
-    clicks.textContent = surveyResults[key].clicks ? surveyResults[key].clicks : 0;
-    percentage.textContent = (clicks.textContent * 100 / impressions.textContent).toFixed(0) + '%';
+    sortResults.forEach(key => {
+        const tr = document.createElement('tr');
+        const product = document.createElement('td');
+        const impressions = document.createElement('td');
+        const clicks = document.createElement('td');
+        const percentage = document.createElement('td');
 
-    tr.append(product, impressions, clicks, percentage);
-    tableBody.appendChild(tr);
-});
+        // console.log(key);
 
-table.appendChild(tableBody);
-container.appendChild(table);
+        product.textContent = key;
+        impressions.textContent = surveyResults[key].impressions ? surveyResults[key].impressions : 0;
+        clicks.textContent = surveyResults[key].clicks ? surveyResults[key].clicks : 0;
+        percentage.textContent = (clicks.textContent * 100 / impressions.textContent).toFixed(0) + '%';
+
+        tr.append(product, impressions, clicks, percentage);
+        tableBody.appendChild(tr);
+    });
+
+    table.appendChild(tableBody);
+
+    const tableFooter = document.createElement('tfoot');
+    const tableFooterRow = document.createElement('tr');
+    const footers = ['Total', sumImpressions(), sumClicks(), getPercentage()];
+
+    footers.forEach(footer => {
+        const td = document.createElement('td');
+        td.textContent = footer;
+        tableFooterRow.appendChild(td);
+    });
+
+    tableFooter.appendChild(tableFooterRow);
+    table.appendChild(tableFooter);
+
+    container.appendChild(table);
+}
+
+let headerSorts = {};
+
+function sortAnalytics(header) {
+    let sortResults;
+
+    if (!headerSorts[header]) headerSorts[header] = {};
+    headerSorts[header].sort = headerSorts[header].sort ? headerSorts[header].sort * -1 : 1;
+
+    document.getElementById('analyticsTable').remove();
+    if (header === 'impressions' || header === 'clicks') {
+        sortResults = headerSorts[header].sort < 0
+            ? Object.keys(surveyResults).sort(function(a, b){return surveyResults[a][header] - surveyResults[b][header];})
+            : Object.keys(surveyResults).sort(function(a, b){return surveyResults[b][header] - surveyResults[a][header];});
+    } else if (header === 'percentage') {
+        sortResults = headerSorts[header].sort < 0
+            ? Object.keys(surveyResults).sort(function(a, b){return (surveyResults[a]['clicks'] / surveyResults[a]['impressions']) - (surveyResults[b]['clicks'] / surveyResults[b]['impressions']);})
+            : Object.keys(surveyResults).sort(function(a, b){return (surveyResults[b]['clicks'] / surveyResults[b]['impressions']) - (surveyResults[a]['clicks'] / surveyResults[a]['impressions']);}); 
+    } else {
+        sortResults = headerSorts[header].sort > 0
+            ? Object.keys(surveyResults).sort()
+            : Object.keys(surveyResults).sort().reverse();
+    }
+
+    renderAnalyticsTable(sortResults);
+    // }
+}
+
+function sumImpressions() {
+    let total = 0;
+    Object.keys(surveyResults).forEach(key => {
+        total = total + surveyResults[key].impressions;
+    });
+    return total;
+}
+
+function sumClicks() {
+    let total = 0;
+    Object.keys(surveyResults).forEach(key => {
+        total = total + surveyResults[key].clicks;
+    });
+    return total;
+}
+
+function getPercentage() {
+    let percentage;
+    percentage = (sumClicks() * 100 / sumImpressions()).toFixed(0) + '%';
+    return percentage;
+}
