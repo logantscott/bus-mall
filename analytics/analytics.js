c36f90bfb2f5d673c51d9ff7ff232ca8190e3e13
Logan Scott <logantscott@gmail.com>
2020-01-29 01:32:28 -0800

    initial analytics results function created, returning expected

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -1,5 +1,20 @@
 const allSessions = JSON.parse(localStorage.getItem('allSessions'));
 
+// example object:
+// wine-glass: {
+    // impressions: 16,
+    // clicks: 3
+// },
+
+let surveyResults = {};
+
 allSessions.forEach(session => {
-    
-})
+    session.productIds.forEach(productId => {
+        if (!surveyResults[productId]) surveyResults[productId] = {};
+        surveyResults[productId].impressions = surveyResults[productId].impressions ? surveyResults[productId].impressions + 1 : 1;
+    });
+    if (!surveyResults[session.selectedId]) surveyResults[session.selectedId] = {};
+    surveyResults[session.selectedId].clicks = surveyResults[session.selectedId].clicks ? surveyResults[session.selectedId].clicks + 1 : 1;
+});
+
+console.log(surveyResults);
